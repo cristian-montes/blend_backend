a0849b6103743e917873ce66fbf47ea324a8e9dc
Cristian Montes <cmontesmujica@gmail.com>
2022-01-01 19:50:45 -0700

    refactored attached cookie to make it work2

--- dist/controllers/auth.js
+++ dist/controllers/auth.js
@@ -2,17 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const UserServices_1 = require("../services/UserServices");
-const attachCookie = (res, theUser) => {
-    res.cookie('session', theUser.authToken(), {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24,
-    });
-};
+// const attachCookie =(res:Response, theUser:any) => {
+//     res.cookie('session', theUser.authToke(),{
+//         httpOnly: true,
+//         maxAge: 1000 * 60 * 60 * 24,
+//     })
+// }
 const authentication = (0, express_1.Router)();
 authentication.post('/signup', async (req, res, next) => {
     try {
         const newUser = await UserServices_1.UserServices.create(req.body);
-        attachCookie(res, newUser);
+        // attachCookie(res, newUser);
+        res.cookie('session', newUser.authToken(), {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 2,
+        });
         res.send(newUser);
     }
     catch (error) {
@@ -22,7 +26,11 @@ authentication.post('/signup', async (req, res, next) => {
 authentication.post('/signin', async (req, res, next) => {
     try {
         const existingUser = await UserServices_1.UserServices.authorize(req.body);
-        attachCookie(res, existingUser);
+        // attachCookie(res, existingUser);
+        res.cookie('session', existingUser.authToken(), {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 2,
+        });
         res.send(existingUser);
     }
     catch (error) {
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,19 +3,23 @@ import ensureAuth from "../middleware/ensureAuth";
 import { User } from "../models/Users";
 import { UserServices } from "../services/UserServices";
 
-const attachCookie =(res:Response, theUser:any) => {
-    res.cookie('session', theUser.authToken(),{
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24,
-    })
-}
+// const attachCookie =(res:Response, theUser:any) => {
+//     res.cookie('session', theUser.authToke(),{
+//         httpOnly: true,
+//         maxAge: 1000 * 60 * 60 * 24,
+//     })
+// }
 
 const authentication = Router();
 
     authentication.post('/signup', async (req:Request, res:Response, next: NextFunction)=>{
         try {
             const newUser = await UserServices.create(req.body);
-            attachCookie(res, newUser);
+            // attachCookie(res, newUser);
+            res.cookie('session', newUser.authToken(),{
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 2,
+            })
             res.send(newUser)
         } catch (error) {
             next(error);
@@ -26,7 +30,12 @@ const authentication = Router();
     authentication.post('/signin', async (req:Request, res:Response, next: NextFunction)=>{
         try {
             const existingUser = await UserServices.authorize(req.body);
-            attachCookie(res, existingUser);
+            // attachCookie(res, existingUser);
+            res.cookie('session', existingUser.authToken(),{
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 2,
+            })
+
             res.send(existingUser)
         } catch (error) {
             next(error);
